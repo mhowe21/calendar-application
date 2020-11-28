1426f7da3da82ab645d2e531b5199a8a78e15e6f
Mason Howe <masonhowe@masons-mbp.lan>
2020-11-28 11:14:20 -0700

    add container draw

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,16 @@ window.onload = function () {
 }
 
 function containerDraw() {
-    let row = $('<div class="row wee"></div>')
-    $("#calendar-container").append(row)
+
+    for (let i = 0; i < 23; i++) {
+        let row = $('<div class="row"></div>')
+        $("#calendar-container").append(row)
+
+        $(row).append('<div class="col-1"></div>')
+        $(row).append('<div class="col-10"></div>')
+        $(row).append('<div> class="col-1"></div>')
+    }
+
+
+
 }
