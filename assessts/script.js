8f2997ef0bbf564eeab2da7c4b3a295ae4d51eb7
Mason A. Howe <mason.a.howe@gmail.com>
2020-11-29 16:06:05 -0700

    add time

--- assessts/script.js
+++ assessts/script.js
@@ -3,22 +3,31 @@ window.onload = function () {
     console.log(today)
 
     $("#currentDay").text(today)
+    // let start = moment().startOf("day")
+    // console.log(start.format("H"))
     containerDraw()
 }
 
 function containerDraw() {
 
-    for (let i = 0; i < 23; i++) {
+    for (let i = 0; i < 24; i++) {
+
+        // make elements
         let row = $('<div class="row"></div>')
         $("#calendar-container").append(row)
 
-        let timeBox = $(row).append('<div class="col-1 time-box"></div>')
+        let timeBox = $(`<div class="col-1 time-box ${i}-hour"></div>`)
+        let timeBoxLabel = $(`<p>${i}:00</p>`)
+        timeBox.append(timeBoxLabel)
+        row.append(timeBox)
         let eventBox = $('<div class="col-10 future-times"></div>')
         $(row).append(eventBox)
         let eventBoxInput = $('<input type="text" id="text" class="text-entry" name="text">') 
         $(eventBox).append(eventBoxInput)       
         let saveBox = $(row).append('<div class="col-1 save-items"></div>')
 
+        // if past the current hour mark it 
+        
 
     }
 
