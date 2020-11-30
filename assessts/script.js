e688e61a2f73df8775d9d03ddb7829d62a65752d
Mason A. Howe <mason.a.howe@gmail.com>
2020-11-29 19:46:48 -0700

    add active hours

--- assessts/script.js
+++ assessts/script.js
@@ -20,13 +20,23 @@ function containerDraw() {
         let timeBoxLabel = $(`<p>${i}:00</p>`)
         timeBox.append(timeBoxLabel)
         row.append(timeBox)
-        let eventBox = $('<div class="col-10 future-times"></div>')
+        let eventBox = $('<div class="col-10"></div>')
         $(row).append(eventBox)
-        let eventBoxInput = $('<input type="text" id="text" class="text-entry" name="text">') 
+        let eventBoxInput = $('<input type="text" class="text-entry" name="text">') 
         $(eventBox).append(eventBoxInput)       
         let saveBox = $(row).append('<div class="col-1 save-items"></div>')
 
         // if past the current hour mark it 
+        let current = moment().format("H")
+        console.log(current)
+
+        if(current > i) {
+            //$(eventBox).addClass("future-times")
+            $(eventBox).addClass("prior-hour")
+        }else{
+            $(eventBox).addClass("future-times")
+        }
+
         
 
     }
